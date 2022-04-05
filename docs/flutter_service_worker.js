e479fb0f3d7cbc91ba251e594125ce7849724ec8
Eugene Kuleshov <ekuleshov@gmail.com>
2022-04-05 00:29:20 -0400

    practiscore demo

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -5,9 +5,9 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "8cffc9a0f1b2a26597bfad1b4531fa5d",
 "favicon.ico": "78d281be46da3d641adf7caf63eb9fb6",
-"index.html": "a77d88e62ffe794d3379912789a922ec",
-"": "a77d88e62ffe794d3379912789a922ec",
-"main.dart.js": "6557e1a56e5d29a824937ef0c494f250",
+"index.html": "d1997157180eef49b0d8dfc7621fc76b",
+"": "d1997157180eef49b0d8dfc7621fc76b",
+"main.dart.js": "4c42cba30a2246d5f783212b17b027f5",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "8a2f35709bacb15e2218814a02e4c711",
